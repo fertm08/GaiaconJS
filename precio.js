f74bc56fb866a181d35c7a1bb09358ddde3f9786
Fer Taype <lfertm08@gmail.com>
2022-07-05 21:06:37 -0300

    Gaia con JS

---
+++ precio.js
@@ -0,0 +1,46 @@
+let nombre = prompt("Ingrese su nombre")
+
+
+console.log("Bienvenido: ", nombre);
+
+let c = 460
+let bm = 3150
+let kjt = 1300
+let mmn = 680
+let cs = 1050
+let mz = 580
+/*function seguir_comprando(){
+    let decision = prompt("¿Desea seguir comprando? SI/NO")
+}
+
+while decision = "SI"*/
+let producto = prompt("Ingrese las iniciales del producto")
+if (producto == "c"){
+    console.log("Su monto a pagar es: ", c,);
+}
+/*if (producto == "c"){
+    console.log("Su monto a pagar es: ", c, seguir_comprando());
+}*/
+else if (producto == "bm") {
+    console.log("Su monto a pagar es: ", bm);
+}
+else if (producto == "kjt"){
+    console.log("Su monto a pagar es: ", kjt);
+}
+else if (producto == "mmn"){
+    console.log("Su monto a pagar es: ", mmn);
+}
+else if (producto == "cs"){
+    console.log("Su monto a pagar es: ", cs);
+}
+else if (producto == cs){
+    console.log("Su monto a pagar es: ", cs);
+}
+else {
+    console.log("Información invalida");
+}
+
+
+
+
+
