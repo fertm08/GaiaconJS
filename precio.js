e4b061983c9c5b647a6754987105be5591487504
Fer Taype <lfertm08@gmail.com>
2022-07-14 23:19:06 -0300

    Evento de precios

--- precio.js
+++ precio.js
@@ -1,46 +1,57 @@
-let nombre = prompt("Ingrese su nombre")
 
+let castaño = { nombre: "Castanea Sativa", precio: 1050}
 
-console.log("Bienvenido: ", nombre);
+let funny_pot = { nombre: "Maceta Zorro", precio: 580}
 
-let c = 460
-let bm = 3150
-let kjt = 1300
-let mmn = 680
-let cs = 1050
-let mz = 580
-/*function seguir_comprando(){
-    let decision = prompt("¿Desea seguir comprando? SI/NO")
-}
+let black_pot = { nombre: "Maceta mediana negra", precio: 680}
 
-while decision = "SI"*/
-let producto = prompt("Ingrese las iniciales del producto")
-if (producto == "c"){
-    console.log("Su monto a pagar es: ", c,);
-}
-/*if (producto == "c"){
-    console.log("Su monto a pagar es: ", c, seguir_comprando());
-}*/
-else if (producto == "bm") {
-    console.log("Su monto a pagar es: ", bm);
-}
-else if (producto == "kjt"){
-    console.log("Su monto a pagar es: ", kjt);
-}
-else if (producto == "mmn"){
-    console.log("Su monto a pagar es: ", mmn);
-}
-else if (producto == "cs"){
-    console.log("Su monto a pagar es: ", cs);
-}
-else if (producto == cs){
-    console.log("Su monto a pagar es: ", cs);
-}
-else {
-    console.log("Información invalida");
+let tools = { nombre:"Kit de jardineria Terra", precio: 1300}
+
+let bonsai = { nombre:"Bonsai moyogi", precio: 3150}
+
+let grass = { nombre:"Césped", precio: 460}
+
+function mostrar_precio(){
+    if (mostrar_precio = 1){
+    console.log(grass.precio)}
+    else if (mostrar_precio = 2){
+    console.log(bonsai.precio);}
+    else if (mostrar_precio = 3){
+    console.log( tools.precio);
+    }
+    else if (mostrar_precio = 4){
+    console.log( black_pot.precio);
+    }
+    else if (mostrar_precio = 5){
+    console.log(funny_pot.precio);
+    }
+    else if (mostrar_precio = 6){
+    console.log(castaño.precio);
+    }
 }
 
 
 
+let boton1 = document.getElementById("grass");
+boton1.preventDefault();
+boton1.addEventListener("onclick" , mostrar_precio(1));{};
 
+let boton2 = document.getElementById("bonsai");
+boton2.preventDefault();
+boton2.addEventListener("onclick" , mostrar_precio(2));{};
 
+let boton3 = document.getElementById("tools");
+boton3.preventDefault();
+boton3.addEventListener("onclick" , mostrar_precio(3));{};
+
+let boton4 = document.getElementById("black_pot");
+boton4.preventDefault();
+boton4.addEventListener("onclick" , mostrar_precio(4));{};
+
+let boton5 = document.getElementById("funny_pot");
+boton5.preventDefault();
+boton5.addEventListener("onclick" , mostrar_precio(5));{};
+
+let boton6 = document.getElementById("castaño");
+boton6.preventDefault();
+boton6.addEventListener("onclick" , mostrar_precio(6));{};
